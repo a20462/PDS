ead7b62658523dce526a1d5f878bcf1c568d9d43
filipesalves <fialves99@gmail.com>
2024-05-24 18:19:10 +0100

    Configurar Front End e Página Inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao Meu Projeto!</h1>
+        <p>Esta é a página inicial do frontend em React.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
