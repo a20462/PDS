3c2bddb10d65a15a3d1f60a7b1acdd06ce7014c0
filipesalves <fialves99@gmail.com>
2024-05-25 18:27:31 +0100

    Adicionar páginas e dar forma á página inicial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './pages/Home';
 import About from './pages/Sobre';
 import Contact from './pages/Contato';
+import Viatura from './pages/Viatura';
+import Oficina from './pages/Oficina';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
+        <nav className="navbar">
+          <div className="logo">
+            <img src="/images/logo.png" alt="SCF Logo" />
+          </div>
+          <ul className="nav-links">
             <li>
-              <Link to="/">Início</Link>
+              <Link to="/viatura">Viatura</Link>
             </li>
             <li>
-              <Link to="/sobre">Sobre Nós</Link>
+              <Link to="/oficina">Oficinas</Link>
             </li>
             <li>
-              <Link to="/contato">Contacto</Link>
+              <Link to="/suporte">Suporte</Link>
+            </li>
+            <li>
+              <Link to="/sobre">Sobre Nos</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/viatura" element={<Viatura />} />
+          <Route path="/oficina" element={<Oficina />} />
+          <Route path="/suporte" element={<Contact />} />
           <Route path="/sobre" element={<About />} />
-          <Route path="/contato" element={<Contact />} />
         </Routes>
+        <header className="header">
+          <img src="./images/background.png" alt="Background" className="background-image" />
+          <div className="overlay">
+            <h1 className="header-title">The pleasure of driving</h1>
+            <img src="/path/to/car.png" alt="Car" className="car-image" />
+          </div>
+        </header>
       </div>
     </Router>
   );
---
+++ frontend/src/pages/Viatura.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Viatura() {
+  return (
+    <div>
+      <h1>Sobre Nós</h1>
+      <p>Esta é a página sobre nós.</p>
+    </div>
+  );
+}
+
+export default Viatura;
